751e16cef65ddaf5aea52e315e49ed8543fc596d
Daniel Hjertholm <daniel.hjertholm@statnett.no>
2022-01-17 22:08:46 +0100

    More tests

--- sylte/_sylte.py
+++ sylte/_sylte.py
@@ -18,7 +18,7 @@ def _ensure_dir_exists(path: Union[str, Path]) -> None:
     if not os.path.exists(path):
         try:
             os.makedirs(path)
-        except OSError as exc:  # Guard against race condition
+        except OSError as exc:  # Guard against race condition. # pragma: no cover
             if exc.errno != errno.EEXIST:
                 raise
 
