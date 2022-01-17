114dae882660d643ec6a65215630adfd8820b407
Daniel Hjertholm <8713259+danhje@users.noreply.github.com>
2022-01-17 12:52:42 +0100

    Refactor and simplify interface

--- tests/test_sylte.py
+++ tests/test_sylte.py
@@ -1,7 +1,8 @@
+import pickle
 from datetime import datetime
 
-from sylte import sylt
-from sylte._sylte import _ensure_dir_exists, _latest, _sylte_time, _Sylted, sylted
+from sylte import sylt, unsylt, show, clear, latest
+from sylte._sylte import _ensure_dir_exists, _sylte_time
 
 
 def test_ensure_dir_exists(tmp_path, monkeypatch):
@@ -16,53 +17,28 @@ def test_sylte_time():
     )
 
 
-def test_latest():
-    sylte_names = [
-        "test_sylte-func-2022-01-14-15-16-39",
-        "test_sylte-func-2022-01-14-15-16-40",
-    ]
-    assert _latest(*sylte_names) == "test_sylte-func-2022-01-14-15-16-40"
-    assert _latest(*reversed(sylte_names)) == "test_sylte-func-2022-01-14-15-16-40"
-
-
-def test_sylted_init(tmp_path, monkeypatch):
+def test_show(tmp_path, monkeypatch):
     monkeypatch.setattr("sylte._sylte.CACHE_DIR", tmp_path)
     open(tmp_path / "test_sylte-func-2022-01-14-15-16-00.pickle", "a").close()
     open(tmp_path / "test_sylte-func-2022-01-14-15-16-01.pickle", "a").close()
 
-    sylted = _Sylted()
-    assert "test_sylte-func-2022-01-14-15-16-00" in sylted.__dict__
-    assert "test_sylte-func-2022-01-14-15-16-01" in sylted.__dict__
-
-
-def test_sylted_call(tmp_path, monkeypatch):
-    monkeypatch.setattr("sylte._sylte.CACHE_DIR", tmp_path)
-    monkeypatch.setattr(
-        "pickle.load",
-        lambda x: "success"
-        if "test_sylte-func-2022-01-14-15-16-00" in str(x)
-        else "fail",
-    )
-    open(tmp_path / "test_sylte-func-2022-01-14-15-16-00.pickle", "a").close()
-    open(tmp_path / "test_sylte-func-2022-01-14-15-16-01.pickle", "a").close()
-
-    sylted = _Sylted()
-    assert sylted("test_sylte-func-2022-01-14-15-16-00") == "success"
-
-
-def test_sylted_list(tmp_path, monkeypatch):
-    monkeypatch.setattr("sylte._sylte.CACHE_DIR", tmp_path)
-    open(tmp_path / "test_sylte-func-2022-01-14-15-16-00.pickle", "a").close()
-    open(tmp_path / "test_sylte-func-2022-01-14-15-16-01.pickle", "a").close()
-
-    sylted = _Sylted()
-    assert sylted.list() == [
+    assert show() == [
         "test_sylte-func-2022-01-14-15-16-00",
         "test_sylte-func-2022-01-14-15-16-01",
     ]
 
 
-def test_sylted_latest(tmp_path, monkeypatch):
+def test_show_with_substring(tmp_path, monkeypatch):
+    monkeypatch.setattr("sylte._sylte.CACHE_DIR", tmp_path)
+    open(tmp_path / "test_sylte-func-2022-01-14-15-16-00.pickle", "a").close()
+    open(tmp_path / "test_sylte-other_func-2022-01-14-15-16-01.pickle", "a").close()
+
+    assert show("other") == [
+        "test_sylte-other_func-2022-01-14-15-16-01",
+    ]
+
+
+def test_latest(tmp_path, monkeypatch):
     monkeypatch.setattr("sylte._sylte.CACHE_DIR", tmp_path)
     monkeypatch.setattr(
         "pickle.load",
@@ -73,24 +49,15 @@ def test_sylted_latest(tmp_path, monkeypatch):
     open(tmp_path / "test_sylte-func-2022-01-14-15-16-00.pickle", "a").close()
     open(tmp_path / "test_sylte-func-2022-01-14-15-16-01.pickle", "a").close()
 
-    sylted = _Sylted()
-    assert sylted("test_sylte-func-2022-01-14-15-16-01") == "success"
+    assert latest("test_sylte-func-2022-01-14-15-16-01") == "success"
 
 
-def test_sylted_clear(tmp_path, monkeypatch):
+def test_clear(tmp_path, monkeypatch):
     monkeypatch.setattr("sylte._sylte.CACHE_DIR", tmp_path)
-    monkeypatch.setattr(
-        "pickle.load",
-        lambda x: "success"
-        if "test_sylte-func-2022-01-14-15-16-01" in str(x)
-        else "fail",
-    )
     open(tmp_path / "test_sylte-func-2022-01-14-15-16-00.pickle", "a").close()
     open(tmp_path / "test_sylte-func-2022-01-14-15-16-01.pickle", "a").close()
 
-    sylted = _Sylted()
-    sylted.clear()
-    assert sylted.list() == []
+    clear()
     assert list(tmp_path.iterdir()) == []
 
 
@@ -104,7 +71,16 @@ def test_sylt_decorated_function_works(tmp_path, monkeypatch):
     assert add(3, 7) == 10
 
 
-def test_sylt_unsylts_to_args(tmp_path, monkeypatch):
+def test_unsylt(tmp_path, monkeypatch):
+    monkeypatch.setattr("sylte._sylte.CACHE_DIR", tmp_path)
+    argset = (1, 2.0), {3: ...}
+    with open(tmp_path / "test_sylte-func-2022-01-14-15-16-00.pickle", "wb") as f:
+        pickle.dump(argset, f)
+
+    assert unsylt("test_sylte-func-2022-01-14-15-16-00") == argset
+
+
+def test_latest_reverses_sylt(tmp_path, monkeypatch):
     monkeypatch.setattr("sylte._sylte.CACHE_DIR", tmp_path)
 
     @sylt
@@ -113,4 +89,4 @@ def test_sylt_unsylts_to_args(tmp_path, monkeypatch):
 
     func(1, [2], c={3: ...})
 
-    assert sylted.latest() == ((1, [2]), {"c": {3: ...}})
+    assert latest() == ((1, [2]), {"c": {3: ...}})
